bad2e4d5706d443e7647ea39d25b659b9c5416db
robby <robby.remmenga@gmail.com>
2020-01-15 19:43:42 -0500

    initial commit

---
+++ src/main/java/com/example/videogamesite/VideoGameSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.videogamesite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoGameSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoGameSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/videogamesite/VideoGameSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.videogamesite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoGameSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
